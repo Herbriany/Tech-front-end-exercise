57083129eccc214d4348e3623ded9f040657f118
Brian Derrig <brian_derrig@hotmail.co.uk>
2021-02-19 13:58:00 +0000

    Added products sourcing APIs and placed products in ManagePDP state

---
+++ src/api/pdpApi.js
@@ -0,0 +1,8 @@
+import { handleResponse, handleError } from "./apiUtils";
+const baseUrl = "https://jsainsburyplc.github.io/front-end-test/products.json"
+
+export function getProducts() {
+    return fetch(baseUrl)
+    .then(handleResponse)
+    .catch(handleError)
+}
--- src/components/productDisplay/ManageProductDisplayPage.js
+++ src/components/productDisplay/ManageProductDisplayPage.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { getProducts } from '../../api/pdpApi'
 
 function ManageProductDisplayPage() {
+
+    useEffect(() => {
+        getProducts().then(_products => setProducts(_products))
+        console.log(products)
+    } , [])
+
+    const [products, setProducts] = useState({})
+
+    
+    
     return (
         <div>
             Product Page
